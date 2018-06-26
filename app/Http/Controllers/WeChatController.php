6a4be618514f32d9d85796b0490d06f1204d7cb7
DavidCao <davidcao@DavidCaodeiMac.local>
2018-06-27 00:49:21 +0800

    Move user model to models folder

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -15,10 +15,39 @@ class WeChatController extends Controller
     public function serve()
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
-
         $app = app('wechat.official_account');
+
         $app->server->push(function ($message) {
-            return "欢迎关注 overtrue1！";
+            switch ($message['MsgType']) {
+        case 'event':
+            return '收到事件消息';
+            break;
+        case 'text':
+            return '收到文字消息';
+
+            break;
+        case 'image':
+            return '对不起，我暂时不能识别图片内容。';
+            break;
+        case 'voice':
+            return $message["Recognition"];
+            break;
+        case 'video':
+            return '收到视频消息';
+            break;
+        case 'location':
+            return '收到坐标消息';
+            break;
+        case 'link':
+            return '收到链接消息';
+            break;
+        case 'file':
+            return '收到文件消息';
+        // ... 其它消息
+        default:
+            return '收到其它消息';
+            break;
+    }
         });
         return $app->server->serve();
     }
